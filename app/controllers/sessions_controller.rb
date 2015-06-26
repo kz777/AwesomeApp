104a1649d28016ae84e81305f9879af37b203c09
ASLAN USSAYEV <usprof777@gmail.com>
2015-06-26 12:13:28 -0500

    Finish log in/log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by(email: params[:session][:email].downcase)
-  	if user && user.authenticate(params[:session][:password])
-  	else
-  		flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
-  		render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      remember user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
     end
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
-
 end
