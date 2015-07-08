275705e1064afa7727afd087d7b795424c4cec31
ASLAN USSAYEV <usprof777@gmail.com>
2015-07-08 18:50:40 -0500

    Change Image upload in production settings

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,11 +5,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [400, 400]
 
 
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
