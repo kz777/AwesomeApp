0ef828cc0db9849d44feef9e85d2e59824dc3531
ASLAN USSAYEV <usprof777@gmail.com>
2015-07-12 11:21:01 -0500

    Set some configs for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,13 +30,13 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
-  # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like
-  # NGINX, varnish or squid.
-  # config.action_dispatch.rack_cache = true
 
-  # Disable serving static files from the `/public` folder by default since
+
+
+config.serve_static_assets = true
+
+
+
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
@@ -45,7 +45,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
