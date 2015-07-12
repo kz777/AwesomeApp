6f557b1426595f208e676be349351bad037808c9
ASLAN USSAYEV <usprof777@gmail.com>
2015-07-12 11:02:50 -0500

    Add some view improvements

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
+             email: "aseke@example.org",
              password:              "foobar",
              password_confirmation: "foobar",
              admin:     true,
